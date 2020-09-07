67a4be482f7f7e9bebbc21f52ed0a4ef9703306f
lmonsalve <lmonsalve22@gmail.com>
2020-09-07 18:18:15 -0300

    funcional

--- AplicacionBlanco/AplicacionBlanco/App_Start/BundleConfig.cs
+++ AplicacionBlanco/AplicacionBlanco/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace AplicacionBlanco
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/Scripts/bootstrap.js"
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/Utilidades.js"
                         ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ AplicacionBlanco/AplicacionBlanco/Controllers/IFRAMEController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionBlanco.Controllers
+{
+    public class IFRAMEController : Controller
+    {
+        // GET: IFRAME
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
