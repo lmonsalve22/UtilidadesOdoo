1e58e744903e0d8d56d8212826398e5bb34aa0f0
Vivian Daniela Romero <61244319+Viviandrg7@users.noreply.github.com>
2021-01-15 15:11:13 -0600

    ICVU

---
+++ AplicacionBlanco/AplicacionBlanco/Controllers/ICVUController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionBlanco.Controllers
+{
+    public class ICVUController : Controller
+    {
+        // GET: ICVU
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult A2017()
+        {
+            return View();
+        }
+
+        public ActionResult A2018()
+        {
+            return View();
+        }
+
+        public ActionResult A2019()
+        {
+            return View();
+        }
+
+        public ActionResult A2020()
+        {
+            return View();
+        }
+
+
+    }
+}
