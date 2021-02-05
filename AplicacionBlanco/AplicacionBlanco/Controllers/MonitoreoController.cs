2d9e95d252e91f3c70c1ca40240bac68c9f3562b
Ian <ianmeza29@gmail.com>
2021-02-05 09:07:40 -0600

    Update

--- AplicacionBlanco/AplicacionBlanco/Controllers/MonitoreoController.cs
+++ AplicacionBlanco/AplicacionBlanco/Controllers/MonitoreoController.cs
@@ -16,5 +16,27 @@ namespace AplicacionBlanco.Controllers
             ViewBag.resultado = db.Solicitudes;
             return View();
         }
+
+        public Logs
+            {
+
+        this.ordenes = new List<Orden>();
+            string url = "https://github.com/Sud-Austral/Monitoreo/raw/main/DisponibilidadActualizado.csv";
+
+        HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+        var reader = new StreamReader(resp.GetResponseStream());
+
+        List<string> listA = new List<string>();
+        List<string> listB = new List<string>();
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+        var values = line.Split(',');
+                this.ordenes.Add(new Orden(values[9], values[23], values[18]));
+                //this.ordenes.Add(new Orden(values[],values[],values[],values[],values[]));
+                //string values_r = values[1].Replace("\"", "");
+                }
+            }
     }
 }
