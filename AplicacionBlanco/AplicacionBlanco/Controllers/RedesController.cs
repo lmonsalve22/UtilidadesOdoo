b3c913ec73276aae58f6388189ed0b0783ed3acf
gholley4 <gholley42@gmail.com>
2021-03-24 16:36:56 -0300

    Intento1DeBarraSocial

---
+++ AplicacionBlanco/AplicacionBlanco/Controllers/RedesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionBlanco.Controllers
+{
+    public class RedesController : Controller
+    {
+        public ActionResult EleccionesRedesHN()
+        {
+            return View();
+        }
+    }
+}
